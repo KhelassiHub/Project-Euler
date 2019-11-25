f80473f61c8c8a34a89d0dd1d9ae460bdef33f51
khelassiHUB <mohamed.khelassi@ens.uvsq.fr>
2019-11-26 00:32:58 +0100

    Solution to Problem 4

--- euler4.py
+++ euler4.py
@@ -3,22 +3,22 @@
 #The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 #Find the largest palindrome made from the product of two 3-digit numbers.
 
-def god_s_will():
-	max = 0
+def main():
+	maximum = 0
 	for a in range(1,999):
 		for b in range(1,999):
 			result = a*b
 			result_str = str(result)
 			if result_str == result_str[::-1] :
-				print(f"palindrome : {result_str}")
-				if max < result :
-					max = result
+				# print(f"palindrome : {result_str}")
+				if maximum < result :
+					maximum = result
 					max_a = a
 					max_b = b
 
-	print (max,a,b)
+	print ("The largest palindrom number is {} for the product of {} and {}".format(maximum,a,b))
 
 if __name__ == "__main__":
     # execute only if run as a script
-    god_s_will()
+    main()
     #main()
